051a255a5a4d0ddd450659cb390e873f8594ab04
tomandrian <tomapple58@gmail.com>
2018-12-12 21:28:45 +0700

    fixed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
+class App extends Component {
+    constructor(){
+        super();
+        this.state={isi1: ""}
+    }
+
+    klik (){
+        var a = this.refs.nilai1.value;
+        var gambar = `https://robohash.org/${a}`
+
+        this.setState({
+            isi1:gambar
+        })
+    }
+        
+    render(){
+        return(
+          <center>
+            <div id="no1" >
+                <input id="ipt" type="text" ref="nilai1" placeholder="Masukan Nama Anda"></input><br/><br/>
+                <button id="btn" class="btn btn-danger" onClick={()=>{this.klik()}} >Buat Profile Picture!</button>
+                <br/><br/>
+                <img id="img" alt='gambar' src={this.state.isi1}></img>
+            </div>
+          </center>
+        )
+    }
+}
 export default App;
